bf4bd2d0444ba4aa54e68334c83fd95029a16a58
rlecheta <rlecheta@livetouch.com.br>
2019-05-26 20:23:48 -0300

    ResponseStatus

--- src/main/java/com/carros/api/exception/ExceptionConfig.java
+++ src/main/java/com/carros/api/exception/ExceptionConfig.java
@@ -2,12 +2,17 @@ package com.carros.api.exception;
 
 import net.bytebuddy.pool.TypePool;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.Serializable;
 
 @RestControllerAdvice
 public class ExceptionConfig extends ResponseEntityExceptionHandler {
@@ -24,5 +29,20 @@ public class ExceptionConfig extends ResponseEntityExceptionHandler {
     })
     public ResponseEntity errorBadRequest(Exception ex) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(new ExceptionError("Operação não permitida"), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+}
+
+class ExceptionError implements Serializable {
+    private String error;
+    ExceptionError(String error) {
+        this.error = error;
+    }
+    public String getError() {
+        return error;
     }
 }
