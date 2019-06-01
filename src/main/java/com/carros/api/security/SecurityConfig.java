9e82702015ef1c88fa7cd990579ab002e0593eb2
rlecheta <rlecheta@livetouch.com.br>
2019-06-01 20:50:49 -0300

    1 - security basics

--- src/main/java/com/carros/api/security/SecurityConfig.java
+++ src/main/java/com/carros/api/security/SecurityConfig.java
@@ -1,46 +1,21 @@
 package com.carros.api.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated().
-            and().httpBasic().
-            and().csrf().disable();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-
-        manager.createUser(getUser("user","user","USER"));
-        manager.createUser(getUser("admin","admin","ADMIN"));
-
-        return manager;
-    }
-
-    private UserDetails getUser(String login, String pwd, String role) {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return User.withUsername(login).password(encoder.encode(pwd)).roles(role).build();
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 }
