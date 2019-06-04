56136dd15920d907a8ba175cfb8bd87ed9347d97
rlecheta <rlecheta@livetouch.com.br>
2019-06-03 23:03:22 -0300

    aws s3

---
+++ src/main/java/com/carros/domain/upload/s3/S3Credencials.java
@@ -0,0 +1,16 @@
+package com.carros.domain.upload.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+
+public class S3Credencials implements AWSCredentials {
+
+	@Override
+	public String getAWSAccessKeyId() {
+		return "AKIAISNX3JGTCIWT33BA";
+	}
+
+	@Override
+	public String getAWSSecretKey() {
+		return "1xONRHOcRlwqh1EuwlPwlzi3DqKtgHCLeYW3RJnN";
+	}
+}
---
+++ src/main/java/com/carros/domain/upload/s3/S3Helper.java
@@ -0,0 +1,56 @@
+package com.carros.domain.upload.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class S3Helper {
+
+	public static final String S3_SERVER = "https://s3-sa-east-1.amazonaws.com";
+	public static final String BUCKET = "udemy-temp";
+
+	public AmazonS3 connect() {
+		AmazonS3 conn = new AmazonS3Client(new S3Credencials() );
+		return conn;
+	}
+
+	public String putFile(String dir, String fileName, String contentType, byte[] bytes) throws IOException {
+		AmazonS3 conn = connect();
+
+		String path = StringUtils.isNotEmpty(dir) ? dir+"/"+fileName : fileName;
+
+		ObjectMetadata metadata = new ObjectMetadata();
+		if(contentType != null) {
+			metadata.setContentType(contentType);
+			metadata.setContentLength(bytes != null ? bytes.length : 0);
+		}
+		
+		if(bytes != null) {
+			conn.putObject(BUCKET, path, new ByteArrayInputStream(bytes), metadata);
+		} else {
+			conn.putObject(BUCKET, path, new ByteArrayInputStream(new byte[]{}),metadata);
+		}
+		
+		// Publico
+		conn.setObjectAcl(BUCKET, path, CannedAccessControlList.PublicRead);
+		
+		String url = getFileURL(dir,fileName);
+		return url;
+	}
+
+	public String getFileURL(String dir, String file) {
+		String url =  S3_SERVER + "/" + BUCKET + "/" +dir+"/"+file;
+		return url;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S3Helper s = new S3Helper();
+		String url = s.putFile("livrows", "nome.txt", "text/plan", "Ricardo Lecheta".getBytes());
+		System.out.println(url);
+	}
+
+}
