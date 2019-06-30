9d47575cff105a99b37c5a0ad1eeb0896024cb8e
rlecheta <rlecheta@livetouch.com.br>
2019-06-30 12:04:25 -0300

    users

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,7 +1,5 @@
 package com.carros.api;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/carros/api/users/UserController.java
+++ src/main/java/com/carros/api/users/UserController.java
@@ -1,16 +1,11 @@
 package com.carros.api.users;
 
-import com.carros.api.infra.security.jwt.JwtLoginInput;
-import com.carros.api.infra.security.jwt.JwtUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
