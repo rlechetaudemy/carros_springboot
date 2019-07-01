66c101f4e8be5039f1ca0b85b71cd6361576d5c2
rlecheta <rlecheta@livetouch.com.br>
2019-06-30 22:17:39 -0300

    swagger

--- src/main/java/com/carros/api/SwaggerConfig.java
+++ src/main/java/com/carros/api/SwaggerConfig.java
@@ -3,13 +3,17 @@ package com.carros.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -21,6 +25,16 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
+                .globalOperationParameters(
+                        Collections.singletonList(
+                                new ParameterBuilder()
+                                        .name("Authorization")
+                                        .description("Bearer token")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build())
+                )
                 .apiInfo(apiInfo());
     }
 
