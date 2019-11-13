154647a93fa4279a897d286a68c1732c669c0b32
rlecheta <rlecheta@livetouch.com.br>
2019-11-13 10:08:41 -0300

    Correção do método PUT / update

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -31,9 +31,7 @@ public class CarrosController {
     @PostMapping
     public String post(@RequestBody Carro carro) {
 
-        carro.setId(null);
-
-        Carro c = service.save(carro);
+        Carro c = service.insert(carro);
 
         return "Carro salvo com sucesso: " + c.getId();
     }
@@ -41,9 +39,7 @@ public class CarrosController {
     @PutMapping("/{id}")
     public String put(@PathVariable("id") Long id,@RequestBody Carro carro) {
 
-        carro.setId(id);
-
-        Carro c = service.save(carro);
+        Carro c = service.update(carro, id);
 
         return "Carro atualizado com sucesso: " + c.getId();
     }
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,8 +25,31 @@ public class CarroService {
         return rep.findByTipo(tipo);
     }
 
-    public Carro save(Carro carro) {
+    public Carro insert(Carro carro) {
+        Assert.isNull(carro.getId(),"Não foi possível inserir o registro");
+
         return rep.save(carro);
+    }
+
+    public Carro update(Carro carro, Long id) {
+        Assert.notNull(id,"Não foi possível atualizar o registro");
+
+        // Busca o carro no banco de dados
+        Optional<Carro> optional = rep.findById(id);
+        if(optional.isPresent()) {
+            Carro db = optional.get();
+            // Copiar as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro id " + db.getId());
+
+            // Atualiza o carro
+            rep.save(db);
+
+            return db;
+        } else {
+            throw new RuntimeException("Não foi possível atualizar o registro");
+        }
     }
 
     public Carro delete(Long id) {
