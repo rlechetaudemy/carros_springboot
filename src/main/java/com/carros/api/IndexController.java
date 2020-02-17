edf024d0a5e049030916926cd97c6d2e40240f76
rlecheta <rlecheta@livetouch.com.br>
2020-02-17 14:12:07 -0300

    aws

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -12,7 +12,7 @@ public class IndexController {
 
     @GetMapping()
     public String get() {
-        return "API dos Carros - Code Commit";
+        return "API dos Carros - AWS !!!";
     }
 
     @GetMapping("/userInfo")
